0cf9bcb3863a9af830f6d648120508e8888ddf26
unknown <>
2016-11-21 19:16:45 +0800

    think a method about cpp

--- blog/views.py
+++ blog/views.py
@@ -16,11 +16,22 @@ def archive(request):
 	return render(request,"archive.html",{'posts':posts,'form':BlogPostForm()})
 	#return render_to_response('archive.html',{'posts':posts},RequestContext(request))
 
+def _saveBody(body):
+	with open("body.cpp",'w') as bd:
+		bd.write(body)
+		bd.write(r"\nover")
+def _readBody(bodyFile):
+	with open(bodyFile,'r') as bd:
+		text=bd.read()
+	return text
 def create_blogpost(request):
 	if request.method=='POST':
 		form=BlogPostForm(request.POST)
 		if form.is_valid():
 			post=form.save(commit=False)
 			post.timestamp=datetime.now()
+			_saveBody(post.body)
+			post.body=_readBody("body.cpp")
 			post.save()
+
 		return HttpResponseRedirect('/blog/')
